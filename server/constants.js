ac1588b5d87018fe1a4e59763cbba4b46d32347e
codewiththehim <codewiththehimanshu@gmail.com>
2024-05-20 22:59:33 +0530

    config

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,7 @@
 
 module.exports = {
-    databaseURL: "mongodb+srv://hj:hj@200120@airone.qcnydgr.mongodb.net/?retryWrites=true&w=majority&appName=airone",
-    port: process.env.PORT || 10000,
+    databaseURL: "mongodb+srv://Airone2024:Airone2024@airone.qcnydgr.mongodb.net/?retryWrites=true&w=majority&appName=airone",
+    port:  10000,
 }
+
+
