c017077c36e44b580e4535c2ce7a22b54e1a64cd
codewiththehim <codewiththehimanshu@gmail.com>
2024-07-10 00:34:07 +0530

    port

--- awsServer/constants.js
+++ awsServer/constants.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     databaseURL: "mongodb+srv://codewiththehimanshu:o09928446853@airone.uvoycvd.mongodb.net/?retryWrites=true&w=majority&appName=Airone",
-    port:  10000,
+    port:  5000,
 }
 
 
